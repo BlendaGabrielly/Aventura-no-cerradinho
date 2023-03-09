5d592dc8d404e2ab7fe45806335bf92f003d9deb
Mateus Barros Macedo <59882435+mateusbmm@users.noreply.github.com>
2023-03-09 18:48:55 -0300

    teste de modificação

--- Aventura-no-cerradinho/Assets/Script/Player.cs
+++ Aventura-no-cerradinho/Assets/Script/Player.cs
@@ -6,9 +6,8 @@ public class Player : MonoBehaviour
 {
     public float velocidade = 8f;
     public float forcaPulo = 8f;
-    
 
-
+    public bool estaPulando;
 
     private Rigidbody2D rig;
 
@@ -32,9 +31,14 @@ public class Player : MonoBehaviour
 
     void movimentos()
     {
-        Vector3 movimento = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movimento * Time.deltaTime * velocidade;
+       
 
+        if (estaPulando)
+        {
+            Vector3 movimento = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+            transform.position += movimento * Time.deltaTime * velocidade;
+
+        }
 
     }
 
@@ -43,14 +47,36 @@ public class Player : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             rig.AddForce(new Vector2(0f,forcaPulo), ForceMode2D.Impulse);
+            
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+
+            estaPulando = false;
+           
+        }
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            estaPulando = true;
 
         }
+
     }
 
 
 
 
 
-    
+
 
 }
