883f35a8cb2da787d9f660193f33a3ebf51119df
Mateus Barros Macedo <59882435+mateusbmm@users.noreply.github.com>
2023-03-11 23:34:04 -0300

    personagem
    Ajustes no script de movimentação e criação de animações de movimento

--- Aventura-no-cerradinho/Assets/Script/Player.cs
+++ Aventura-no-cerradinho/Assets/Script/Player.cs
@@ -7,15 +7,17 @@ public class Player : MonoBehaviour
     public float velocidade = 8f;
     public float forcaPulo = 8f;
 
-    public bool estaPulando;
+    public bool estaPulando, segundoPulo;
 
     private Rigidbody2D rig;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
 
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();    
    
 
     }
@@ -36,16 +38,35 @@ public class Player : MonoBehaviour
             Vector3 movimento = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
             transform.position += movimento * Time.deltaTime * velocidade;
 
+        if(Input.GetAxis("Horizontal") >= 0)
+        {
+            anim.SetBool("Andando", true);
+        }
+        else
+        {
+            anim.SetBool("Andando", false);
+        }
+            
         
 
     }
 
     void pulo()
     {
-        if (Input.GetButtonDown("Jump") && !estaPulando)
+        if (Input.GetButtonDown("Jump"))
         {
-            rig.AddForce(new Vector2(0f,forcaPulo), ForceMode2D.Impulse);
+
+
+            if (!estaPulando)
+            {
+                rig.AddForce(new Vector2(0f, forcaPulo), ForceMode2D.Impulse);
+                segundoPulo = true;
+            }
+            else if(segundoPulo) { 
             
+                rig.AddForce(new Vector2(0f, forcaPulo), ForceMode2D.Impulse);
+                segundoPulo = false;
+            }
         }
     }
 
@@ -58,7 +79,8 @@ public class Player : MonoBehaviour
         {
             
             estaPulando = false;
-           
+            anim.SetBool("Pulando", false);
+
         }
         
     }
@@ -68,7 +90,7 @@ public class Player : MonoBehaviour
         if (collision.gameObject.layer == 7)
         {
             estaPulando = true;
-            
+            anim.SetBool("Pulando", true);
 
         }
 
